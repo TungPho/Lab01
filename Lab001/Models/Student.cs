a58fb83e99ce481e190d061c3e31a0b21ac63bb6
Phó Tùng <ductungpho1005@gmail.com>
2023-09-27 14:26:48 +0700

    Hello2

--- Lab001/Models/Student.cs
+++ Lab001/Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
@@ -8,7 +9,7 @@ namespace Lab001.Models
 	{
         public int Id { get; set; }//Mã sinh viên
         
-        [Required]
+        [Required(ErrorMessage = "Bắt buộc phải nhập tên")]
         public string? Name { get; set; } //Họ tên
         
         [Required(ErrorMessage = "Email bắt buộc phải được nhập")]
@@ -17,27 +18,31 @@ namespace Lab001.Models
         
         
         [StringLength(100, MinimumLength = 8)]
-        [Required]
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc và có độ dài ít nhất 8 ký tự")]
         public string? Password { get; set; }//Mật khẩu
         
-        [Required]
+        [Required(ErrorMessage = "Ngành học bắt buộc phải nhập")]
         public Branch? Branch { get; set; }//Ngành học
         
-        [Required]
+        [Required(ErrorMessage = "Giới tính bắt buộc phải được nhập")]
         public Gender? Gender { get; set; }//Giới tính
         
         public bool IsRegular { get; set; }//Hệ: true-chính quy, false-phi chính quy
         
         [DataType(DataType.MultilineText)]
-        [Required]
+        [Required(ErrorMessage = "Địa chỉ bắt buộc phải được nhập")]
         public string? Address { get; set; }//Địa chỉ
         
         [Range(typeof(DateTime), "1/1/1963", "12/31/2005")]
         [DataType(DataType.Date)]
-        [Required]
+        [Required(ErrorMessage = "Ngày sinh bắt buộc phải được nhập")]
         public DateTime DateOfBorth { get; set; }//Ngày sinh
 
-        [AllowNull]
-        public IFormFile ImgFile { get; set; } //Upload Picture
+        [Required(ErrorMessage = "Điểm bắt buộc phải được nhập từ 0.0 tới 10.0")]
+		[Range(0.0, 10.0)]
+        [Column(TypeName ="decimal(2,2)")]		
+        public decimal Points { get; set; }
+
+       
     }
 }
